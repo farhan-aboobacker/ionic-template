2479e5b357041691389057794552f272cae552cb
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-03 13:13:10 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -10,8 +10,15 @@
 
         echo "Installing ionic cli..."
         npm install -g @ionic/cli
-        
+        echo "Ionic cli Installed"
 
+        echo "Creating project..."
+        ionic start "$out" tabs
         
+        mkdir "$out"/.idx
+        cp ${./dev.nix} "$out"/.idx/dev.nix
+        chmod -R +w "$out"
+       
+        (cd "$out"; npm install --package-lock-only --ignore-scripts)
     '';
 }
