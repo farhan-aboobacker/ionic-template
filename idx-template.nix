c967b9182f9c6392fa44a5f79c5aa9c2ad43ded8
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-03 16:06:41 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -10,9 +10,7 @@
     mkdir -p "$out/.idx"
 
     chmod -R u+w "$out"
-    cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-    cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-    chmod -R u+w "$out"
+   
 
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
   '';
