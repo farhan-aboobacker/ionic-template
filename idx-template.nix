821e122413168d3e762725a760a1b4b35e552ee6
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-03 15:46:43 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -13,7 +13,7 @@
          npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
          mkdir -p "$out"/.idx
          cp ${./dev.nix} "$out"/.idx/dev.nix
-         mv "$WS_NAME" "$out"
+        # mv "$WS_NAME" "$out"
     
          chmod -R u+w "$out"
        
