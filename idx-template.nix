4728cfb2aa4b90662d9f7996a5389234e7bceae0
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-03 17:16:49 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -2,10 +2,10 @@
   channel = "stable-24.11";
   packages = [ pkgs.nodejs_20 ];
   bootstrap = ''
-    mkdir "$out"/.idx
+    mkdir -p "$WS_NAME"/.idx
     echo "out :: $out"
     echo "workspace :: $WS_NAME"
-    cp ${./dev.nix} "$out"/.idx/dev.nix
+    cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix
     npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
     mv "$WS_NAME" "$out"
     chmod -R u+w "$out"
