6da4718bf2ee61adb85fdbfc22b8acc08b302784
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-06 10:19:28 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   channel = "stable-24.11";
-  packages = [ pkgs.nodejs_24 ];
+  packages = [ pkgs.nodejs_20 ];
   bootstrap = ''
     echo "out :: $out"
     echo "workspace :: $WS_NAME"
