117314385e2fc38342b99adc7d1c65dced1c23be
farhan.aboobacker <farhan.aboobacker@cognizant.com>
2025-10-06 10:11:42 +0000

    update nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,48 +1,24 @@
-{ pkgs, type ? "angular", ... }:
-
-let
-
-  # Map user input to valid Ionic project types
-
-  typeMap = {
-
-    angular = "angular";
-
-    react   = "react";
-
-    vue     = "vue";
-
-  };
-
-  # Pick type from map, default to "angular" if unknown
-
-  projectType = builtins.getAttrOr "angular" type typeMap;
-
-in
-
-{
-
+{ pkgs, type ? angular, ... }: {
+  channel = "stable-25.05";
   packages = [
-
-    pkgs.nodejs_24
-
+    pkgs.nodejs_24 
     pkgs.git
-
   ];
-
   bootstrap = ''
-
-    echo "Bootstrapping Ionic project with type: ${projectType}"
-
-    npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=$projectType --no-deps --no-git --no-link --no-interactive
-
+    echo "Environment ${type}"
+    if [ "${type}" = "angular" ]; then
+          npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
+    elif [ "${type}" = "react" ]; then
+          npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=react --no-deps --no-git --no-link --no-interactive
+    elif [ "${type}" = "vue" ]; then
+          npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=vue --no-deps --no-git --no-link --no-interactive
+    else
+          npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
+    fi
+    cp -rf ${./.}/${type} "$WS_NAME"
     chmod -R +w "$WS_NAME"
-
     mv "$WS_NAME" "$out"
-
+    chmod -R u+w "$out"
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
-
   '';
-
 }
- 
