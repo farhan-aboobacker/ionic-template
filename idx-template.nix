b309774b4d890d448e3c178edf7cdfda65c2e9d0
farhan.aboobacker <farhan.aboobacker@cognizant.com>
2025-10-06 10:51:17 +0000

    update folder structure

--- idx-template.nix
+++ idx-template.nix
@@ -15,9 +15,10 @@
     else
           npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
     fi
-    cp -rf ${./.}/${type} "$WS_NAME"
+    cp -rf ${./.}/${type}/. "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
+    
     chmod -R u+w "$out"
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
   '';
