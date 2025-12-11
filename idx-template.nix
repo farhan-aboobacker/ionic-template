3e5b298a183493484218bde3fc1cbfe75913afe2
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-12-11 20:12:27 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -7,10 +7,9 @@
   bootstrap = ''
     echo "Environment ${type}"
     npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type="${type}" --no-deps --no-git --no-link --no-interactive
-    cp -rf ${./.}/${type}/. "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
-    
+    packageManager=${packageManager} j2 ${./devNix.j2} -o "$out/.idx/dev.nix"
     chmod -R u+w "$out"
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
   '';
